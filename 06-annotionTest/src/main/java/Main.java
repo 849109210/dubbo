e8141295df314de47b46fdce5dd36d6335f1c2ad
zhangjie <zhangjie@163.com>
2021-07-24 16:53:02 +0800

    添加注释

--- 06-annotionTest/src/main/java/Main.java
+++ 06-annotionTest/src/main/java/Main.java
@@ -5,6 +5,7 @@ import service.TestService;
  * 张杰
  * 2021/7/24
  */
+//采用注解的方式进行开发
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("test.xml");
