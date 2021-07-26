f68556ef7a52e56c3f70c040a61a916f73c27a1e
zhangjie <zhangjie@163.com>
2021-07-26 17:15:05 +0800

    新建菜单

---
+++ 01-common/src/main/java/model/Menu.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * 张杰
+ * 2021/7/26
+ */
+public class Menu {
+    private String name;
+    private String description;
+}
---
+++ 01-common/src/main/java/service/MyMenu.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.Menu;
+
+/**
+ * 张杰
+ * 2021/7/26
+ */
+public interface MyMenu {
+    Menu ckeckMenu(String name,String description);
+}
